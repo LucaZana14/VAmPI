638e12b8e004d171ac107d812e6a6bf4245c6bf8
LucaZana14 <luczanardi02@gmail.com>
2026-05-28 10:56:27 +0200

    Trigger Mobb test

--- bad_secrets.py
+++ bad_secrets.py
@@ -3,7 +3,7 @@ my_secret_api_key = "a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0"
 
 # Esca per Chiave Asimmetrica (Modificata per sembrare finta a GitHub, ma gitLeaks legge l'intestazione)
 fake_key = """
------BEGIN PRIVATE KEY-----
+-----BEGIN PRIVATE KEYs-----
 MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDK...
 -----END PRIVATE KEY-----
 """
